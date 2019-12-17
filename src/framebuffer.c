4d9dbd01afe7eb8d3efae0481ad1cddca53b1021
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2019-12-17 17:35:54 +0300

    a bit of refactorign

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -42,6 +42,7 @@ static void fb_prepare_texture(t_mlx_win_framebuffer* fb)
 	glGenBuffers(1, &fb->ebo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, fb->ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glUniform1i(glGetUniformLocation(fb->shader_program, "tex"), 0);
 }
 
 void t_mlx_win_framebuffer_init(t_mlx_win_framebuffer* fb, int w, int h)
@@ -49,14 +50,11 @@ void t_mlx_win_framebuffer_init(t_mlx_win_framebuffer* fb, int w, int h)
 	fb->w = w;
 	fb->h = h;
 	fb->data = calloc(fb->w * fb->h, sizeof(uint));
-	fb_prepare_texture(fb);
-
 	fb->shader_program = link_shaders(
 			2,
 			make_shader(GL_VERTEX_SHADER, "shaders/vertex.glsl"),
 			make_shader(GL_FRAGMENT_SHADER, "shaders/fragment.glsl"));
-	glUseProgram(fb->shader_program);
-	glUniform1i(glGetUniformLocation(fb->shader_program, "tex"), 0);
+	fb_prepare_texture(fb);
 }
 
 void t_mlx_win_framebuffer_draw(t_mlx_win_framebuffer* fb)
